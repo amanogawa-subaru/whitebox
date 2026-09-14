63bdf58f874f402f518acf1addd35d3ade11b576
Amanogawa Subaru <amanogawa.subaru0705@gmail.com>
2026-09-14 18:31:15 +0800

    split shared system config with bedrock

--- imports.nix
+++ imports.nix
@@ -1,31 +1,9 @@
-{ lib, ... }:
-
-let
-  settings = import ./settings.nix;
-  username = settings.username;
-in
-
 {
-  _module.args = {
-    inherit username settings;
-  };
-
   imports = [
     ./modules/system.nix
     ./modules/desktop.nix
     
     ./modules/core-packages.nix
     ./modules/default-packages.nix
-    ./modules/user-packages.nix
-    
-    ./modules/mount.nix
-    
-    ./modules/fonts.nix
-  ]
-  ++ lib.optional
-    settings.nvidia
-    ./modules/nvidia.nix
-  ++ lib.optional
-    settings.nvidiaPrime
-    ./modules/nvidia-prime.nix;
+  ];
 }
--- modules/system.nix
+++ modules/system.nix
@@ -1,44 +1,7 @@
-# This module is intended for system settings
-{ pkgs, username, ... }:
+# This module is intended for Whitebox-specific system configuration
+{ username, ... }:
 
 {
-  # Enable flakes
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ]; 
- 
-  # Enable trash
-  services.gvfs.enable = true;
-  
-  # Graphics
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # Audio
-  security.rtkit.enable = true;
-  
-  services.pipewire = {
-    enable = true;
-    
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    # PulseAudio compatibility
-    pulse.enable = true;
-  };
-  
-  # Gaming
-  programs.gamemode.enable = true;
-  hardware.steam-hardware.enable = true;
-
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
   # Home Manager
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
@@ -46,37 +9,11 @@
   home-manager.extraSpecialArgs = {
     inherit username;
   };
-  
-  home-manager.users.${username}.imports = [ 
+
+  home-manager.users.${username}.imports = [
     ../home
   ];
 
-  # I2C support for external monitor control
-  hardware.i2c.enable = true;
-  
   # PAM for lockscreen
   security.pam.services.hyprlock = {};
-  
-  # Japanese input
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    
-    extraLocales = [
-      "ja_JP.UTF-8/UTF-8"
-    ];
-    
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-
-      fcitx5 = {
-		waylandFrontend = true;  
-		  
-        addons = with pkgs; [
-          fcitx5-mozc
-          fcitx5-gtk
-        ];
-      };
-    };  
-  };
 }
