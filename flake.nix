9bf0902eb14046f5a4d9c73b2bdf15344e56c4ff
Amanogawa Subaru <amanogawa.subaru0705@gmail.com>
2026-09-14 23:36:36 +0800

    remove profile-owned nixpkgs input

--- flake.nix
+++ flake.nix
@@ -2,17 +2,8 @@
   description = "whitebox NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    catppuccin = {
-      url = "github:catppuccin/nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    catppuccin.url = "github:catppuccin/nix";
   };
   
   outputs = { home-manager, catppuccin, ... }: {
